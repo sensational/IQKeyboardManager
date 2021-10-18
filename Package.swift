70c23affe125c31306cc9b743db24847c2e1ef45
Amadeu Andrade <amadeu.dev@gmail.com>
2021-10-18 09:16:57 +0200

    make possible to archive apps on Xcode 13/iOS 15

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "IQKeyboardManagerSwift",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
        .library(name: "IQKeyboardManagerSwift", targets: ["IQKeyboardManagerSwift"])
    ],
